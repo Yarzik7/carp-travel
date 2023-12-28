ef702fe2ea044eb61b70575ba0ba9ece79bfa1be
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-12-28 19:33:07 +0200

    bagfix and adaptive for hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,34 @@
 import Section from "../Section";
+import { title, places, slogan, description } from "../../data/hero/hero.json";
 import css from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <Section sectionClasses={[css.heroSection, css.heroBgImg].join(" ")}>
       <div className="w-full h-full md:relative md:flex justify-between">
-        <div className="mb-[24px] text-right uppercase md:absolute top-[0px] right-[20px]  ">
-          <p className="text-37px font-thin tracking-1.65px leading-none">
-            <span className="font-medium leading-none">7</span>Days
+        <div className="smOnly:mb-[24px] text-right uppercase md:absolute top-[0px] -right-[14px]">
+          <p className="text-[37px] font-thin tracking-[1.65px] leading-none md:text-[67px] md:tracking-[8.71px] lg:text-[98px] lg:tracking-[3px]">
+            <span className="font-medium">{slogan[0]}</span>
+            {slogan[1]}
           </p>
-          <p className="max-w-[295px] font-light text-12px tracking-[9.48px] leading-none md:tracking-[25.9px] lg:tracking-[36.48px]">
-            Journey
+          <p className="max-w-[295px] font-light text-[12px] tracking-[9.48px] md:absolute md:left-[5px] md:text-[14px] md:tracking-[27px] lg:text-[16px] lg:tracking-[36.48px]">
+            {slogan[2]}
           </p>
         </div>
 
-        <div className="flex flex-col gap-[24px] mb-[24px]">
+        <div className="flex flex-col gap-[24px] smOnly:mb-[24px] mdOnly:w-[426px]">
           <h1 className="font-thin -tracking-[1.6px] uppercase text-[40px] md:mb-[68px] md:w-[426px] md:text-[67px] lg:mb-[148px] lg:w-[646px] lg:text-[98px]">
-            <span className="font-medium">Uncover</span> Carpathian’s Secrets
+            <span className="font-medium">{title[0]}</span> {title[1]}
           </h1>
 
-          <p className="w-[59%] text-[10px] font-extralight leading-1.6 md:w-[62%] md:text-[14px] md:leading-[1.14] md:tracking-[1.26px] lg:w-[100%] lg:text-[16px] lg:leading-[1.5] lg:tracking-[1.44px]">
-            Hoverla / Yaremche / Zakarpattia / Vorokhta / Synevyr Lake / Bukovel
+          <p className="w-[59%] text-[10px] font-extralight leading-1.6 md:w-[62%] md:text-[13px] md:leading-[1.14] md:tracking-[1.26px] lg:w-[100%] lg:text-[16px] lg:leading-[1.5] lg:tracking-[1.44px]">
+            {places}
           </p>
         </div>
 
-        <div className="w-full self-end md:w-[230px] lg:[294px]">
-          <p className="mb-[24px] text-justify font-extralight leading-[1.42]">
-            We offer you unforgettable trips to the most beautiful parts of the
-            Carpathians. Enjoy stunning views, exciting expeditions, and the
-            best service!
+        <div className="w-full self-end md:w-[230px] lg:w-[294px]">
+          <p className="mb-[24px] text-justify font-extralight leading-[1.42] md:text-[16px] md:leading-[1.25] lg:text-[18px]">
+            {description}
           </p>
           <button className="uppercase text-18px flex justify-center items-center font-bold w-full h-53px bg-bgButton">
             Join now
