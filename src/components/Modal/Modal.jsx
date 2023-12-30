eb402dde7c4820bfcef5f0829a078c490703ebee
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2023-12-30 11:56:40 +0200

    fix modal error

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,28 @@
 "use client";
+import { useEffect, useState } from "react";
 import Backdrop from "./Backdrop";
 import ModalContent from "./ModalContent";
 import { createPortal } from "react-dom";
 
-const modalRoot = document.querySelector("#modal-root");
-
 const Modal = ({ onClose, children }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  const modalRoot = document.getElementById("modal-root");
+
   return createPortal(
-    <Backdrop onClose={onClose}>
-      <ModalContent>{children}</ModalContent>
-    </Backdrop>,
+    <>
+      <Backdrop onClose={onClose}>
+        <ModalContent>{children}</ModalContent>
+      </Backdrop>
+    </>,
     modalRoot
   );
 };
